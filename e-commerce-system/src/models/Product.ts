7b5c7a988d7d9ad9d8a98d1535d84a61f4b423ab
scriptMang <aperalta686@gmail.com>
2026-02-07 22:13:48 -0500

    Add product class to product file

---
+++ e-commerce-system/src/models/Product.ts
@@ -0,0 +1,24 @@
+class Product {
+    id: number ;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercantage: number;
+    rating: number;
+    stock: number;
+    tags: string[];
+    brand: string;
+    sku: string;
+    weight: number;
+    dimensions:  object; // need a dimension interface type object
+    warrantyInfo: string;
+    shippingInfo: string;
+    availabilityStatus: string;
+    reviews: string[]; // need a review interface type object
+    returnPolicy: string;
+    minOrderQuantity:  number;
+    meta: object; // need a meta interface type object
+    thumbnail: string;
+    images: string[];
+}
