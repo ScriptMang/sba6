f0dd091dff982aab6024831ea43b08814809b1ef
scriptMang <aperalta686@gmail.com>
2026-02-08 00:48:43 -0500

    Add constructor to product class

--- e-commerce-system/src/models/Product.ts
+++ e-commerce-system/src/models/Product.ts
@@ -25,7 +25,7 @@ class Product {
     description: string;
     category: string;
     price: number;
-    discountPercantage: number;
+    discountPercentage: number;
     rating: number;
     stock: number;
     tags: string[];
@@ -42,4 +42,36 @@ class Product {
     _meta: meta;
     thumbnail: string;
     images: string[];
+    constructor(
+        id: number, title: string, description: string,
+        category: string, price: number, discountPercentage: number,
+        rating: number, stock: number, tags: string[], 
+        brand: string, sku: string, weight: number,
+        dimensions: dimension, warrantyInfo: string, shippingInfo: string,
+        availabilityStatus: string, reviews: review[], returnPolicy: string,
+        minOrderQuantity: number, _meta: meta,thumbnail: string, images: string[]
+    ){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.stock = stock;
+        this.tags = tags;
+        this.brand = brand;
+        this.sku = sku;
+        this.weight = weight;
+        this.dimensions = dimensions;
+        this.warrantyInfo = warrantyInfo;
+        this.shippingInfo = shippingInfo;
+        this.availabilityStatus = availabilityStatus;
+        this.reviews = reviews;
+        this.returnPolicy = returnPolicy;
+        this.minOrderQuantity = minOrderQuantity;
+        this._meta = _meta;
+        this.thumbnail = thumbnail;
+        this.images = images;
+    }
 }
