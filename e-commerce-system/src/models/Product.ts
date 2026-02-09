79379c33385dc1f06a46ea260d6a8f09f7ccda0d
scriptMang <aperalta686@gmail.com>
2026-02-09 01:22:12 -0500

    Add back construct to product class

--- e-commerce-system/src/models/Product.ts
+++ e-commerce-system/src/models/Product.ts
@@ -42,6 +42,42 @@ class Product {
     _meta: Meta;
     thumbnail: string;
     images: string[];
+
+    constructor(
+        id: number, title: string, description: string,
+        category: string, price: number, discountPercentage: number,
+        rating: number, stock: number, tags: string[],
+        brand: string, sku: string, weight: number,
+        dimensions:  Dimension, warrantyInfo: string, shippingInfo: string,
+        availabilityStatus: string, reviews: Review[], returnPolicy: string,
+        minOrderQuantity:  number, _meta: Meta, thumbnail: string,
+        images: string[]
+    ){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.stock = stock;
+        this.tags = tags;
+        this.brand = brand;
+        this.sku = sku;
+        this.weight = weight;
+        this.dimensions = dimensions;
+        this.warrantyInfo = warrantyInfo;
+        this.shippingInfo = shippingInfo;
+        this.availabilityStatus = availabilityStatus;
+        this.reviews = reviews;
+        this.returnPolicy = returnPolicy;
+        this.minOrderQuantity = minOrderQuantity;
+        this._meta = _meta;
+        this.thumbnail = thumbnail;
+        this.images = images;
+    }
+
+
 }
 
 
